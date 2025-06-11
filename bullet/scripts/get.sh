62b0b2d4913c9fff3a2a5e5f258ed2e413727e5b
zhen <zhen@Mac.Home>
2025-06-11 16:21:22 +0100

    curl to port

--- bullet/scripts/get.sh
+++ bullet/scripts/get.sh
@@ -1,4 +1,5 @@
-curl -X POST http://localhost:8080/get \
+echo "port is $1"
+curl -X POST http://localhost:$1/get \
   -H "Content-Type: application/json" \
   -d '{"appId":1,"bucketId":42,"key":"mykey"}'
 
--- bullet/scripts/post.sh
+++ bullet/scripts/post.sh
@@ -1,3 +1,4 @@
-curl -X POST http://localhost:8080/put \
+echo "port is $1"
+curl -X POST http://localhost:$1/put \
   -H "Content-Type: application/json" \
   -d '{"appId":1,"bucketId":42,"key":"mykey","value":123456}'
