94c7370a3ac6e972e10c817185b73d8237c0e2b9
zhen <zhen@Mac.Home>
2025-06-15 17:25:47 +0100

    start no cd

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd "$(dirname "${BASH_SOURCE[0]}")"
 # ./build.sh .bullet_bin && BULLET_PORT=80 BULLET_DB_TYPE=mongodb BOLT_PATH=data.db ./start.sh .bullet_bin
 if [ -z "$1" ]
   then
         echo "You must provide a binary name"
         exit 1
 fi
+echo "Bullet starting on $1 and we are in $(eval pwd)"
 
 ./$1 -port $BULLET_PORT -db-type $BULLET_DB_TYPE -mongo $MONGO_PASS -bolt $BOLT_PATH
