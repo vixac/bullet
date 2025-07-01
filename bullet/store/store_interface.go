8dd977eb07bc54a242c47e6739a8698c38d0ace0
zhen <zhen@Mac.Home>
2025-07-01 22:53:19 +0100

    metric and tag are in there now

--- bullet/store/store_interface.go
+++ bullet/store/store_interface.go
@@ -3,14 +3,19 @@ package store
 import "github.com/vixac/bullet/model"
 
 type BucketStore interface {
-	BucketPut(appID int32, bucketID int32, key string, value int64) error
+	BucketPut(appID int32, bucketID int32, key string, value int64, tag *int64, metric *float64) error
 	BucketGet(appID int32, bucketID int32, key string) (int64, error)
 	BucketDelete(appID int32, bucketID int32, key string) error
 	BucketClose() error //VX:TODO whats this
 	BucketPutMany(appID int32, items map[int32][]model.BucketKeyValueItem) error
-	BucketGetMany(appID int32, keys map[int32][]string) (map[int32]map[string]int64, map[int32][]string, error)
-
-	GetItemsByKeyPrefix(appID, bucketID int32, prefix string) ([]model.BucketKeyValueItem, error)
+	BucketGetMany(appID int32, keys map[int32][]string) (map[int32]map[string]model.BucketValue, map[int32][]string, error)
+	GetItemsByKeyPrefix(
+		appID, bucketID int32,
+		prefix string,
+		tags []int64, // optional slice of tags
+		metricValue *float64, // optional metric value
+		metricIsGt bool, // "gt" or "lt"
+	) ([]model.BucketKeyValueItem, error)
 }
 type PigeonStore interface {
 	PigeonPut(appID int32, key int64, value string) error
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # ./build.sh .bullet_bin && BULLET_PORT=80 BULLET_DB_TYPE=mongodb BOLT_PATH=data.db ./start.sh .bullet_bin
+
+#cd bullet then...
 # go run ./cmd/bullet -bolt boltdb -mongo $MONGO_PASS -db-type mongodb -port 10 
 if [ -z "$1" ]
   then
